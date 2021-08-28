81d835e4ce12c0d03afe01b91e867fb91452b9ca
Jeremy Chang <jeremrc1@uci.edu>
2021-08-27 22:28:05 -0700

    Finish simple pomodoro from YouTube

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Pomodoro from './Pomodoro';
+
+// https://www.youtube.com/watch?v=9z1qBcFwdXg 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pomodoro />
     </div>
   );
 }
